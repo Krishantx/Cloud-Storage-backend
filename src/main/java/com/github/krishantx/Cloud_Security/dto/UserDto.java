016514c4e6c05c653855bf6d1b72c3a8afb4dbb8
Pushpak-Jaiswal <pushpakmjaiswal@gmail.com>
2026-01-22 22:25:41 +0530

    added user dto

---
+++ src/main/java/com/github/krishantx/Cloud_Security/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.github.krishantx.Cloud_Security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private long id;
+
+    private String username;
+
+    private List<String> roles;
+
+}
